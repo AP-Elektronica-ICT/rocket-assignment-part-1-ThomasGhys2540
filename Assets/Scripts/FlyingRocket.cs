fcc664770e1a413e7abe84420c8110d3a53b128f
Thomas <s112981@ap.be>
2021-03-19 11:30:54 +0100

    Uploading Unity project

---
+++ Assets/Scripts/FlyingRocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingRocket : MonoBehaviour
+{
+    public int Speed;
+    public Rigidbody Rocket;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rocket.AddForce(new Vector3(0, Speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Rocket.AddForce(new Vector3(-Speed * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Rocket.AddForce(new Vector3(Speed * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Rocket.AddForce(new Vector3(0, 0, Speed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Rocket.AddForce(new Vector3(0, 0, -Speed * Time.deltaTime));
+        }
+    }
+}
