6bf69c88b6585a8c7fe52b0086b23b80d40a4ddd
Thomas <s112981@ap.be>
2021-03-27 13:46:06 +0100

    Update to Level 1

--- Assets/Scripts/FlyingRocket.cs
+++ Assets/Scripts/FlyingRocket.cs
@@ -4,34 +4,32 @@ using UnityEngine;
 
 public class FlyingRocket : MonoBehaviour
 {
-    public int Speed;
     public Rigidbody Rocket;
+    public float Speed;
+    public float Rotation;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Rocket.AddForce(new Vector3(0, Speed * Time.deltaTime, 0));
+            Rocket.AddRelativeForce(new Vector3(0, Speed * Time.deltaTime, 0));
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Rocket.AddForce(new Vector3(-Speed * Time.deltaTime, 0, 0));
+            Rocket.transform.Rotate(0, 0, Rotation * Time.deltaTime, Space.Self);
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Rocket.AddForce(new Vector3(Speed * Time.deltaTime, 0, 0));
+            Rocket.transform.Rotate(0, 0, -Rotation * Time.deltaTime, Space.Self);
         }
-
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            Rocket.AddForce(new Vector3(0, 0, Speed * Time.deltaTime));
+            Rocket.transform.Rotate(Rotation * Time.deltaTime, 0, 0, Space.Self);
         }
-
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            Rocket.AddForce(new Vector3(0, 0, -Speed * Time.deltaTime));
+            Rocket.transform.Rotate(-Rotation * Time.deltaTime, 0, 0, Space.Self);
         }
     }
 }
